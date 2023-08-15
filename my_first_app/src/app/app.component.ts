c1e744b19fd1db50a32e0325c83473b19ce369c5
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-08-15 13:58:06 -0700

    New course about Angular

---
+++ my_first_app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>{{ title }}</h1>'
+})
+export class AppComponent implements OnInit {
+  title: string = '';
+  
+  constructor(){
+  }
+
+  ngOnInit() {
+    this.title = 'Hello World'
+  }
+}
