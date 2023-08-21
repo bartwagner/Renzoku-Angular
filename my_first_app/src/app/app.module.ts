6458318d1912a3ed810b1196ccb3ead8c1944432
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-08-21 15:19:23 -0700

    Created a new components

--- my_first_app/src/app/app.module.ts
+++ my_first_app/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CustomersModule } from './customers/customers.module';
 import { AppComponent }  from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, CustomersModule, SharedModule ],
+  declarations: [ AppComponent  ],
+  bootstrap:    [ AppComponent  ]
 })
-export class AppModule { }
+export class AppModule {}
